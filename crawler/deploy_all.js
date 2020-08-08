1067f1086f0363290b1b41d7b0c4848d5e78d76c
Nikolai Tschacher <nikolai.tschacher@informatik.hu-berlin.de>
2020-08-08 15:14:12 +0200

    updated versions

--- crawler/deploy_all.js
+++ crawler/deploy_all.js
@@ -20,19 +20,19 @@ function deploy_crawl_worker() {
     console.log(systemSync(`npm run build`));
 
     let regions = [
-        // 'us-west-1',
-        // 'us-west-2',
-        'us-east-2',
-        'us-east-1',
-        'eu-central-1',
-        'eu-west-1',
-        'eu-west-2',
-        'eu-west-3',
-        'ap-northeast-1',
-        'ap-northeast-2',
-        'ap-south-1',
-        'ap-southeast-1',
-        'ap-southeast-2',
+        'us-west-1',
+        'us-west-2',
+        // 'us-east-2',
+        // 'us-east-1',
+        // 'eu-central-1',
+        // 'eu-west-1',
+        // 'eu-west-2',
+        // 'eu-west-3',
+        // 'ap-northeast-1',
+        // 'ap-northeast-2',
+        // 'ap-south-1',
+        // 'ap-southeast-1',
+        // 'ap-southeast-2',
     ];
 
     for (let region of regions) {
